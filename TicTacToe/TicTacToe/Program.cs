20b5c7a3b77612a8c82a02d90b1b12ba3b845503
maritall101 <49980381+maritall101@users.noreply.github.com>
2019-05-09 16:05:05 +0200

    Add files via upload

---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Model _model = new Model();
+            Controller _controller = new Controller(_model);
+            View aGame = new View(_controller);
+
+            Application.Run(new View());
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/View.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class View : Form, IObserver
+    {
+        Controller _controller;
+
+        public View(Controller controller)
+        {
+            InitializeComponent();
+            _controller = controller;
+        }
+
+        public void button_click(object sender, EventArgs e)
+        {
+            Button temp = (Button)sender;
+            string tempTag = temp.Tag.ToString();
+            _controller.button_pressed(tempTag);
+        }
+
+        public void restart_click(object sender, EventArgs e)
+        {
+
+        }
+
+        void IObserver.Update(object Model)
+        {
+            Model model = (Model)Model;
+        }
+    }
+}
